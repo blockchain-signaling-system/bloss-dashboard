c2ca8d21be047850c2fe4983c5293755c85a241b
christiankiller <christian.killer@uzh.ch>
2018-10-13 21:44:31 +0200

    #1 enable starting and stopping services from dashboard. also correctly displays status within 2-3 seconds

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ import socketio from 'socket.io-client';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+import store from './store/store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import { Badge, Alert } from 'ant-design-vue';
+import { Badge, Alert, Switch, Button } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css'; 
 
 Vue.component(Badge.name, Badge);
 Vue.component(Alert.name, Alert);
+Vue.component(Switch.name, Switch);
+Vue.component(Button.name, Button);
 
 export const SocketInstance = socketio('http://localhost:3001');
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,64 @@
-/* eslint-disable */
 <template>
-<div>
-  <a-alert
-        message="Websocket connection lost"
-        description="Trying to reconnect."
-        type="warning"
-        showIcon
-        v-if="!isConnected"
-      />
-
-<el-container v-loading="!isConnected">
-  <el-main><div>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
   <div>
-<el-row>
-<el-row>
-  <el-tag :type="isConnected ? 'success' : 'danger'">Websocket</el-tag>
-</el-row>
-  <el-button :type="serviceStatusBloss ? 'success' : 'danger'" size="mini" >BLOSS</el-button>
-  <el-button :type="serviceStatusGeth ? 'success' : 'danger'" size="mini" >GETH</el-button>
-  <el-button :type="serviceStatusIPFS ? 'success' : 'danger'" size="mini" >IPFS</el-button>
-  <el-button :type="serviceStatusInfluxDB ? 'success' : 'danger'" size="mini" >INFLUXDB</el-button>
-</el-row>
+    <a-alert
+          message="Websocket connection lost"
+          description="Trying to reconnect."
+          type="warning"
+          showIcon
+          v-if="!isConnected"
+      />
+    <el-container v-loading="!isConnected">
+      <el-main>
 
-
-    <p v-if="isConnected">We're connected to the server!</p>
-    <p v-else>We're NOT connected to the server!</p>
-    <p>Message from server: "{{socketMessage}}"</p>
-    <p>Message from server: "{{statusMessage}}"</p>
-    <button @click="getUptime()">Get Uptime</button>
-    <button @click="getStatus()">Get Status</button>
+          <div>
+            <el-tag :type="isConnected ? 'success' : 'danger'">Websocket</el-tag>
+            <a-badge :status="isConnected ? 'success' : 'error'" text="WEBSOCKET" />  
+            <br />
+            <a-badge :status="serviceStatusBloss ? 'success' : 'error'" text="BLOSS" />  
+            <el-tag  :type="serviceStatusBloss ? 'success' : 'danger'" size="small">BLOSS</el-tag>
+            <el-button @click="killService('bloss')" size="mini" round >Stop</el-button>
+            <el-button @click="startService('bloss')" size="mini" round>Start</el-button>
+            <br />
+            <a-badge :status="serviceStatusGeth ? 'success' : 'error'" text="GETH" />
+            <el-tag  :type="serviceStatusGeth ? 'success' : 'danger'" size="small">GETH</el-tag>
+            <el-button @click="killService('geth')" size="mini" round >Stop</el-button>
+            <el-button @click="startService('geth')" size="mini" round>Start</el-button>
+            <br />
+            <a-badge :status="serviceStatusIPFS ? 'success' : 'error'" text="IPFS" />
+            <el-tag  :type="serviceStatusIPFS ? 'success' : 'danger'" size="small">IPFS</el-tag>
+            <el-button @click="killService('ipfs')" size="mini" round >Stop</el-button>
+            <el-button @click="startService('ipfs')" size="mini" round>Start</el-button>
+            <br />
+            <a-badge :status="serviceStatusInfluxDB ? 'success' : 'error'"/>
+            <el-tag  :type="serviceStatusInfluxDB ? 'success' : 'danger'" size="small">INFLUXDB</el-tag>
+            <el-button @click="killService('influxdb')" size="mini" round >Stop</el-button>
+            <el-button @click="startService('influxdb')" size="mini" round>Start</el-button>
+            <br />
+          </div>
+    
+        <div>
+    <a-button type="primary" ghost>Primary</a-button>
+    <a-button ghost>Default</a-button>
+    <a-button type="dashed" ghost>Dashed</a-button>
+    <a-button type="danger" ghost>danger</a-button>
   </div>
-  </div></el-main>
-</el-container>
+        <el-row>
+        <el-button :type="serviceStatusBloss ? 'success' : 'danger'" size="mini" >BLOSS</el-button>
+        <el-button :type="serviceStatusGeth ? 'success' : 'danger'" size="mini" >GETH</el-button>
+        <el-button :type="serviceStatusIPFS ? 'success' : 'danger'" size="mini" >IPFS</el-button>
+        <el-button :type="serviceStatusInfluxDB ? 'success' : 'danger'" size="mini" >INFLUXDB</el-button>
+        </el-row>
 
+        <el-row>
+        <p v-if="isConnected">We're connected to the server!</p>
+        <p v-else>We're NOT connected to the server!</p>
+        <p>Message from server: "{{socketMessage}}"</p>
+        <p>Message from server: "{{statusMessage}}"</p>
+        <button @click="getUptime()">Get Uptime</button>
+        <button @click="getStatus()">Get Status</button>
+        </el-row>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -74,14 +96,57 @@ export default {
     },
     // Fired when the server sends something on the "messageChannel" channel.
     messageChannel(data) {
+      console.log(data);
       this.socketMessage = data;
     },
     statusChannel(data) {
+      if (data.geth != null) { 
+        if(data.geth === "active")
+          this.updateStatus("geth", true);
+        if(data.geth === "inactive")
+          this.updateStatus("geth",false);
+      }
+      if (data.ipfs != null) { 
+        if(data.ipfs === "active")
+          this.updateStatus("ipfs", true);
+        if(data.ipfs === "inactive")
+          this.updateStatus("ipfs",false);
+      }
+      if (data.bloss != null) { 
+        if(data.bloss === "active")
+          this.updateStatus("bloss", true);
+        if(data.bloss === "inactive")
+          this.updateStatus("bloss",false);
+      }
+
+      console.log(data); 
       this.statusMessage = data;
+
     }
   },
-
   methods: {
+    updateStatus(service, status) {
+      switch (service) {
+        case 'geth':
+          this.serviceStatusGeth = status;    
+          break;
+        case 'ipfs':
+          this.serviceStatusIPFS = status;    
+          break;
+        case 'bloss':
+          this.serviceStatusBloss = status;    
+          break;
+        default:
+          console.log("no match");
+          break;
+      }
+    },
+    changeStatus(status) {
+      this.serviceStatusBloss = status.bloss;
+      this.serviceStatusGeth = status.geth;
+      this.serviceStatusIPFS = status.ipfs;
+      this.serviceStatusInfluxDB = status.influxdb;
+    },
     getUptime() {
       // Send the "pingServer" event to the server.
       this.$socket.emit("getUptime", "uptime");
@@ -89,6 +154,12 @@ export default {
     getStatus() {
       // Send the "pingServer" event to the server.
       this.$socket.emit("getStatus", "status");
+    },
+    startService(servicename) {
+      this.$socket.emit("serviceCtl", { cmd: "start", service: servicename });
+    },
+    killService(servicename) {
+      this.$socket.emit("serviceCtl", { cmd: "stop", service: servicename });
     }
   },
   name: "home",
